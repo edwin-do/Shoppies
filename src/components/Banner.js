a7980643a772a4af1fcd341147d949b80ef7ff76
edwin-do <doedwin123@gmail.com>
2021-01-09 15:41:38 -0500

    add font-awesome icons

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,13 @@
 import React, {useState} from 'react'
 import Button from 'react-bootstrap/Button'
 import Modal from 'react-bootstrap/Modal'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook } from '@fortawesome/free-brands-svg-icons'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faLinkedin} from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope} from '@fortawesome/free-solid-svg-icons'
+
+
 function Banner(props) {
   const [show, setShow] = useState(true);
   const handleClose = () => setShow(false);
@@ -20,6 +27,10 @@ function Banner(props) {
             <a href="https://twitter.com/intent/tweet?url=https://www.google.com/&text=Nominate your favourite movies for the upcoming Shoppies!" target="blank" rel="noreferrer noopener" alt="share">Twitter</a>
             <a href="https://www.linkedin.com/shareArticle?mini=true&url=https://www.google.com/&title=&summary=Nominate your favourite movies for the upcoming Shoppies! &source=Shoppies" target="blank" rel="noreferrer noopener" alt="share">LinkedIn</a>
             <a href="mailto:info@example.com?&subject=&body=https://www.google.com/ Nominate your favourite movies for the upcoming Shoppies! " target="blank" rel="noreferrer noopener" alt="share">Email</a>           
+            <FontAwesomeIcon icon={faFacebook} />
+            <FontAwesomeIcon icon={faTwitter} />
+            <FontAwesomeIcon icon={faLinkedin} />
+            <FontAwesomeIcon icon={faEnvelope} />
           </div>
         </Modal.Body>
         <Modal.Footer>
