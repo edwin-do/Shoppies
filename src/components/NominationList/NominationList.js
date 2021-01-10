6e99ca667423dbf41bb82c72025313cba32afc85
edwin-do <doedwin123@gmail.com>
2021-01-10 18:01:08 -0500

    add button to clear list

--- src/components/NominationList/NominationList.js
+++ src/components/NominationList/NominationList.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import Result from "../ResultsList/Result";
+import Button from 'react-bootstrap/Button'
 
 const NominationList = props =>{ 
+
+  function clearListHandler(){
+    props.setList([]);
+  }
 
     return (
       <div>
@@ -11,7 +16,12 @@ const NominationList = props =>{
         <Result key={i} id={movie[2]} isNominated={true} results={props.results} setResults={props.setResults} list={props.list} setList={props.setList} Title={movie[0]} Year={movie[1]} ></Result>
         )
         : <p className="text-center mt-5">Remember those movies you like? Yeah, they belong here :) </p>}
-        {props.list.length > 0 & props.list.length < 5 ? <p className="text-center mt-3">You can still nominate {5 - props.list.length} movies</p> : null}
+        {props.list.length > 0 & props.list.length < 5 
+        ? <div>
+            <p className="text-center mt-3">You can still nominate {5 - props.list.length} movies</p>
+            <Button className="text-center" onClick={clearListHandler}>Clear List</Button>
+          </div>
+        : null}
       </div>
       )
 }
