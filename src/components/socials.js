e9d3f7f1665601cecb1b9f1b66972a18d00f2847
edwin-do <doedwin123@gmail.com>
2021-01-11 23:44:52 -0500

    change url for shareable links in banner

--- src/components/socials.js
+++ src/components/socials.js
@@ -1,25 +1,25 @@
 export const socials = [
     {
         name: "facebook",
-        url: "https://www.facebook.com/sharer/sharer.php?u=https://www.google.com/",
+        url: "https://www.facebook.com/sharer/sharer.php?u=https://edwin-do-shoppies.netlify.app/",
         color: "#3b5998",
         icon: ['fab','facebook']    
     },
     {
         name: "Twitter",
-        url: "https://twitter.com/intent/tweet?url=https://www.google.com/&text=Nominate your favourite movies for the upcoming Shoppies!",
+        url: "https://twitter.com/intent/tweet?url=https://edwin-do-shoppies.netlify.app/&text=Nominate your favourite movies for the upcoming Shoppies!",
         color: "#00acee",
         icon: ['fab','twitter'],
     },
     {
         name: "LinkedIn",
-        url: "https://www.linkedin.com/shareArticle?mini=true&url=https://www.google.com/&title=&summary=Nominate your favourite movies for the upcoming Shoppies! &source=Shoppies",
+        url: "https://www.linkedin.com/shareArticle?mini=true&url=https://edwin-do-shoppies.netlify.app//&title=&summary=Nominate your favourite movies for the upcoming Shoppies! &source=Shoppies",
         color: "#0e76a8",
         icon: ['fab','linkedin'], 
     },
     {
         name: "Email",
-        url: "mailto:info@example.com?&subject=&body=https://www.google.com/ Nominate your favourite movies for the upcoming Shoppies! ",
+        url: "mailto:info@example.com?&subject=&body=https://edwin-do-shoppies.netlify.app/ Nominate your favourite movies for the upcoming Shoppies! ",
         color: "#b3b3b3",
         icon: ['fas','envelope'] ,
     },
