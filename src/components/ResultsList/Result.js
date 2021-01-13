9c57d5c864980acbc8495f1bbb99a1097abcd0ef
edwin-do <doedwin123@gmail.com>
2021-01-13 03:23:58 -0500

    add image

--- src/components/ResultsList/Result.js
+++ src/components/ResultsList/Result.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from 'react'
+import Image from 'react-bootstrap/Image'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const result = props =>{ 
 
     function addNomination (){
         // adds nomination to the list and refreshes results
-        props.setList((list) => [...list, [props.Title,props.Year,props.id]]);
+        props.setList((list) => [...list, [props.Title,props.Year,props.id,props.poster]]);
         props.setResults((results) => [...results]);
       }
 
@@ -20,19 +23,24 @@ const result = props =>{
 return (
     <div className="card bg-dark">
       <div className="card-body">
-        <p className="card-title">{props.Title} (<span className="card-text">{props.Year}</span>)
-          { !props.isNominated 
-          ? <button className="btn float-right bg-transparent button-small text-light" 
-                    disabled={props.limitReached} 
-                    onClick={!props.limitReached ? addNomination : null}>
-                      <FontAwesomeIcon icon={"plus-circle"} />
-            </button> 
-          : <button className=" btn float-right bg-transparent button-small text-danger" 
-                    onClick={() => removeNomination(props.id)}>
-                      <FontAwesomeIcon icon={"trash-alt"} />
-            </button>
-          } 
-        </p>
+          <Row>
+            <Col sm={3}><Image src={props.poster} rounded fluid /></Col>
+            <Col sm={9} >
+            <p className="card-title">{props.Title} (<span className="card-text">{props.Year}</span>)
+            { !props.isNominated 
+                  ? <button className="btn float-right bg-transparent button-small text-light" 
+                            disabled={props.limitReached} 
+                            onClick={!props.limitReached ? addNomination : null}>
+                              <FontAwesomeIcon icon={"plus-circle"} />
+                    </button> 
+                  : <button className=" btn float-right bg-transparent button-small text-danger" 
+                            onClick={() => removeNomination(props.id)}>
+                              <FontAwesomeIcon icon={"trash-alt"} />
+                    </button>
+                  } 
+            </p>
+            </Col>
+          </Row>
       </div>
     </div> 
  )}
--- src/components/ResultsList/ResultsList.js
+++ src/components/ResultsList/ResultsList.js
@@ -8,6 +8,8 @@ const resultsList = props =>{
   // checks if results exist
   if (props.results){
 
+    // console.log(props.results);
+
     // check limit and if it exists in nomination list
     return (
       <div>
@@ -15,13 +17,13 @@ const resultsList = props =>{
         {props.results.map((result,i) =>
         
           {if (props.list.length >= 5){
-            return (<Result key={i} id={result.imdbID} limitReached={true} isNominated={false} results={props.results} setResults={props.setResults} list={props.list} setList={props.setList} Title={result.Title} Year={result.Year}></Result>)
+            return (<Result key={i} id={result.imdbID} limitReached={true} isNominated={false} poster={result.Poster} results={props.results} setResults={props.setResults} list={props.list} setList={props.setList} Title={result.Title} Year={result.Year}></Result>)
           }
             else if (props.list.some(movie => movie[2] === result.imdbID)) {
-              return (<Result key={i} id={result.imdbID} isNominated={true} results={props.results} setResults={props.setResults} list={props.list} setList={props.setList} Title={result.Title} Year={result.Year}></Result>)
+              return (<Result key={i} id={result.imdbID} isNominated={true} poster={result.Poster} results={props.results} setResults={props.setResults} list={props.list} setList={props.setList} Title={result.Title} Year={result.Year}></Result>)
           }
           else{
-            return (<Result key={i} id={result.imdbID} isNominated={false} results={props.results} setResults={props.setResults} list={props.list} setList={props.setList} Title={result.Title } Year={result.Year}></Result>)
+            return (<Result key={i} id={result.imdbID} isNominated={false} poster={result.Poster} results={props.results} setResults={props.setResults} list={props.list} setList={props.setList} Title={result.Title } Year={result.Year}></Result>)
           }}
         
         )}
